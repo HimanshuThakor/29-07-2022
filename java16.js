26d96bccefe0c8e2987ec581c54064622fffc6af
Himanshuthakor3584 <104884496+Himanshuthakor3584@users.noreply.github.com>
2022-07-29 14:37:43 +0530

    Add files via upload

---
+++ java16.js
@@ -0,0 +1,17 @@
+
+
+function demoOnFocus(){
+    document.getElementById("txtFocus").style.backgroundColor="Red";
+}
+
+function demoOnBlur(){
+    document.getElementById("txtBlur").style.backgroundColor="yellow";
+}
+
+function demoOnKeypress(){
+    alert("Keypress Done");
+}
+
+function demoOnKeyUp(){
+    alert("KeyUp Done");
+}
